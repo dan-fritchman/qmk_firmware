31ec5d7e21fb201b464d74bd2bdf4d15711092d1
Dan Fritchman <dan@fritch.mn>
2022-07-10 16:47:35 -0700

    Add fritch_ortho1 keyboard

--- keyboards/keebio/quefrency/rev1/config.h
+++ keyboards/keebio/quefrency/rev1/config.h
@@ -35,7 +35,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define MATRIX_COL_PINS { F5, F6, F7, B1, B3, B2, B6, B5 }
 #define MATRIX_ROW_PINS_RIGHT { F4, D4, D7, E6, B4, B5 }
 #define MATRIX_COL_PINS_RIGHT { F5, F6, F7, B1, B3, B2, B6, C6 }
-#define SPLIT_HAND_PIN D2
+
+#define MASTER_LEFT
+//#define SPLIT_HAND_PIN D2
 
 #define DIODE_DIRECTION COL2ROW
 
